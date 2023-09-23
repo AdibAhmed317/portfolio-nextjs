8b4a5f92613c827a2b1a05e0cdbecf0688ba5fdc
AdibAhmed317 <ahmedadib317@gmail.com>
2023-09-23 18:15:51 +0600

    context api

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Intro from '@/components/Intro';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,8 +23,10 @@ export default function RootLayout({
         className={`${inter.className} bg-slate-100 text-gray-950 relative pt-28 sm:pt-36`}>
         <div className='bg-[#fbebe2] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] md:h-screen w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#d7d9fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] md:h-screen w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
-        <Navbar />
-        {children}
+        <ActiveSectionContextProvider>
+          <Navbar />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
